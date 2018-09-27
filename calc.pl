6aecfcf947a2b38019faee27f1b3722e8f669971
Andrew Shitov <andy@shitov.ru>
2018-09-27 18:49:04 +0200

    Attempt to allow .5

--- calc.pl
+++ calc.pl
@@ -4,11 +4,14 @@ grammar Calculator {
     }
 
     token number {
-        <[+-]>? \d+ ['.' \d+]?
+        <[+-]>? \d* ['.' \d+]?
     }
 }
 
-my @cases = 7, 77, -84, '+7', 0, 3.14;
+my @cases =
+    7, 77, -84, '+7', 0,
+    3.14, -2.78, 5.0, '.5',
+    '', '-', '+';
 for @cases -> $expression {
     my $test = Calculator.parse($expression);
     say ($test ?? 'OK ' !! 'NOT OK ') ~ $expression;
