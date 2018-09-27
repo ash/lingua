bc969201c05022ef5f92cb46d655aa32892ea109
Andrew Shitov <andy@shitov.ru>
2018-09-27 17:25:51 +0200

    Calculator parses non-negative integers

---
+++ calc.pl
@@ -0,0 +1,15 @@
+grammar Calculator {
+    rule TOP {
+        <number>
+    }
+
+    token number {
+        \d+
+    }
+}
+
+my @cases = 7, 77, -84;
+for @cases -> $expression {
+    say "Test $expression";
+    say Calculator.parse($expression);
+}
