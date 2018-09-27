1b59fd47853c7b9fc3c6a83bc970768b6693c0be
Andrew Shitov <andy@shitov.ru>
2018-09-27 12:39:24 +0200

    Will use modules soon

--- lingua.pl
+++ lingua.pl
@@ -1,56 +1,5 @@
-my %var;
-
-grammar Lingua {
-    rule TOP {
-        <statement>* %% ';'
-    }
-
-    rule statement {
-        | <variable-declaration>
-        | <assignment>
-        | <function-call>
-    }
-
-    rule variable-declaration {
-        'my' <variable-name>
-    }
-
-    rule assignment {
-        <variable-name> '=' <value>
-    }
-
-    rule function-call {
-        <function-name> <variable-name>
-    }
-
-    token variable-name {
-        \w+
-    }
-
-    token value {
-        \d+
-    }
-
-    token function-name {
-        'say'
-    }
-}
-
-class LinguaActions {
-    method variable-declaration($/) {
-        %var{$<variable-name>} = 0;
-    }
-
-    method assignment($/) {
-        %var{~$<variable-name>} = +$<value>;
-    }
-
-    method function-call($/) {
-        say %var{$<variable-name>} if $<function-name> eq 'say';
-    }
-}
+use Lingua;
+use LinguaActions;
 
 my $code = 'test2.lng'.IO.slurp();
-my $result = Lingua.parse($code, :actions(LinguaActions));
-#say $result;
-#say %var;
+Lingua.parse($code, :actions(LinguaActions));
