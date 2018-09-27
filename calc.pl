c36deef5a8ee192a964306efc31ff1663da74daf
Andrew Shitov <andy@shitov.ru>
2018-09-27 20:11:05 +0200

    Parse numbers like 3.14E2

--- calc.pl
+++ calc.pl
@@ -8,6 +8,7 @@ grammar Calculator {
             | \d+
             | \d* ['.' \d+]
             | \d+ <[eE]> <[+-]>? \d+
+            | \d* ['.' \d+] <[eE]> <[+-]>? \d+
         ]
     }
 }
@@ -16,6 +17,7 @@ my @cases =
     7, 77, -84, '+7', 0,
     3.14, -2.78, 5.0, '.5', '-5.3', '-.3',
     '3E4', '-33E55', '3E-3', '-1E-2',
+    '3.14E2', '.5E-3',
     '', '-', '+';
 for @cases -> $expression {
     my $test = Calculator.parse($expression);
