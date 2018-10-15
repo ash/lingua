618082930dee48b0c0da2b2700b28b9ff035f3a8
Andrew Shitov <andy@shitov.ru>
2018-10-15 14:32:23 +0200

    Array and hash declaration and item assignment

--- LinguaEvaluator.pm
+++ LinguaEvaluator.pm
@@ -11,10 +11,34 @@ class LinguaEvaluator {
         dd %!var;
     }
 
+    # Variables
+
     multi method eval-node(AST::ScalarDeclaration $node) {
         %!var{$node.variable-name} =
             $node.value ~~ AST::Null ?? 0 !! $node.value.value;
     }
+
+    multi method eval-node(AST::ScalarAssignment $node) {
+        %!var{$node.variable-name} = $node.rhs.value;
+    }
+
+    multi method eval-node(AST::ArrayDeclaration $node) {
+        %!var{$node.variable-name} = Array.new;
+    }
+
+    multi method eval-node(AST::HashDeclaration $node) {
+        %!var{$node.variable-name} = Hash.new;
+    }
+
+    multi method eval-node(AST::ArrayItemAssignment $node) {
+        %!var{$node.variable-name}[$node.index] = $node.rhs.value;
+    }
+
+    multi method eval-node(AST::HashItemAssignment $node) {
+        %!var{$node.variable-name}{$node.key} = $node.rhs.value;
+    }
+
+    # Functions
 
     multi method eval-node(AST::FunctionCall $node) {
         self.call-function($node.function-name, $node.value);
