1d0b6526477c0a386582dbed906cdfc33f79a972
Andrew Shitov <andy@shitov.ru>
2018-10-15 13:21:46 +0200

    Implementation of "say" for AST::Variable

--- LinguaEvaluator.pm
+++ LinguaEvaluator.pm
@@ -12,7 +12,15 @@ class LinguaEvaluator {
     }
 
     multi method eval-node(AST::ScalarDeclaration $node) {
-        %!var{$node.variable-name} = 
+        %!var{$node.variable-name} =
             $node.value ~~ AST::Null ?? 0 !! $node.value.value;
+    }
+
+    multi method eval-node(AST::FunctionCall $node) {
+        self.call-function($node.function-name, $node.value);
+    }
+
+    multi method call-function('say', AST::Variable $value) {
+        say %!var{$value.variable-name};
     }
 }
