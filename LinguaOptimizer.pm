5ec2254837fec7a0ae9d3469537c3bd703ed16c3
Andrew Shitov <mail@andreyshitov.com>
2026-07-12 19:15:00 +0200

    Add the AST optimizer that does nothing yet

---
+++ LinguaOptimizer.pm
@@ -0,0 +1,100 @@
+use LinguaAST;
+
+class LinguaOptimizer {
+    method optimize(AST::TOP $top) {
+        return AST::TOP.new(
+            statements => self.optimize-list($top.statements),
+        );
+    }
+
+    method optimize-list(@statements) {
+        my @result;
+        for @statements -> $statement {
+            @result.push(self.optimize-node($statement));
+        }
+        return @result;
+    }
+
+    # Nodes with child values
+
+    multi method optimize-node(AST::ScalarDeclaration $node) {
+        return AST::ScalarDeclaration.new(
+            variable-name => $node.variable-name,
+            value => self.optimize-node($node.value),
+        );
+    }
+
+    multi method optimize-node(AST::ScalarAssignment $node) {
+        return AST::ScalarAssignment.new(
+            variable-name => $node.variable-name,
+            rhs => self.optimize-node($node.rhs),
+        );
+    }
+
+    multi method optimize-node(AST::FunctionCall $node) {
+        return AST::FunctionCall.new(
+            function-name => $node.function-name,
+            argument => self.optimize-node($node.argument),
+            evaluator => $node.evaluator,
+        );
+    }
+
+    multi method optimize-node(AST::UserFunctionCall $node) {
+        return AST::UserFunctionCall.new(
+            function-name => $node.function-name,
+            arguments => ($node.arguments.map: { self.optimize-node($_) }),
+            evaluator => $node.evaluator,
+        );
+    }
+
+    multi method optimize-node(AST::Return $node) {
+        return AST::Return.new(
+            value => self.optimize-node($node.value),
+        );
+    }
+
+    multi method optimize-node(AST::MathOperations $node) {
+        return AST::MathOperations.new(
+            operators => $node.operators,
+            operands => ($node.operands.map: { self.optimize-node($_) }),
+        );
+    }
+
+    # Nodes with statement lists
+
+    multi method optimize-node(AST::Condition $node) {
+        return AST::Condition.new(
+            value => self.optimize-node($node.value),
+            statements => self.optimize-list($node.statements),
+            antistatements => self.optimize-list($node.antistatements),
+        );
+    }
+
+    multi method optimize-node(AST::Loop $node) {
+        return AST::Loop.new(
+            variable => $node.variable,
+            statements => self.optimize-list($node.statements),
+        );
+    }
+
+    multi method optimize-node(AST::While $node) {
+        return AST::While.new(
+            value => self.optimize-node($node.value),
+            statements => self.optimize-list($node.statements),
+        );
+    }
+
+    multi method optimize-node(AST::FunctionDefinition $node) {
+        return AST::FunctionDefinition.new(
+            function-name => $node.function-name,
+            parameters => $node.parameters,
+            statements => self.optimize-list($node.statements),
+        );
+    }
+
+    # Everything else stays as is
+
+    multi method optimize-node(ASTNode $node) {
+        return $node;
+    }
+}
